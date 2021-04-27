1265a9b4394fb56372f02b1ba93920c4a8ef347a
Robiul-Hasan-developer <83179892+Robiul-Hasan-developer@users.noreply.github.com>
2021-04-27 17:45:59 +0600

    Add files via upload

---
+++ PSD to HTML/js/main.js
@@ -0,0 +1,29 @@
+// bx slider=======================================
+
+//$(document).ready(function(){
+   // $('.bx_slider').bxSlider();
+   // $('#navigation_area').meanmenu({
+       // meanMenuContainer: '#mobile_menu',
+        //meanScreenWidth: 991,
+   // });
+//});
+
+
+
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+    $('#navigation_area').meanmenu({
+        meanMenuContainer: '#mobile_menu',
+        meanScreenWidth: '991',
+    });
+  });
+
+
+
+
+
+
+
+
+
+
